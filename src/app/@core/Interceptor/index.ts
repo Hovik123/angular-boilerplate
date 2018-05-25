6a2d6d9b26ce512c8e6326c0702a0406dc283b8f
Hovhannes <hovhannespost@gmail.com>
2018-05-25 16:37:26 +0400

    upgrade to angular 6

--- src/app/@core/Interceptor/index.ts
+++ src/app/@core/Interceptor/index.ts
@@ -1,17 +1,14 @@
+import { throwError as observableThrowError, Observable } from 'rxjs';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class JWTInterceptor implements HttpInterceptor {
 
-  constructor() {
-  }
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).catch(err => {
       if (err instanceof HttpErrorResponse) {
-        return Observable.throw(err);
+        return observableThrowError(err);
       }
     });
   }
